b2baa76559b50459e44db3e20de49bb0af8f5129
kddKena <stas.khlabystov.05@gmail.com>
2021-12-19 10:36:17 +0200

    v0.0.0

---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+    "format_version": 2,
+    "header": {
+      "description": "NymazonMC v0.0.0",
+      "name": "Nyamazon",
+      "uuid":"c55f6382-60a6-11ec-8607-0242ac130002",
+      "version": [1, 0, 0],
+      "min_engine_version": [1, 16, 0]
+    },
+    "modules":
+      [
+        {
+          "description": "My First Add-On!",
+            "type": "data",
+            "uuid": "c55f6602-60a6-11ec-8607-0242ac130002",
+            "version": [1, 0, 0]
+        }
+      ],
+    "dependencies": [
+      {
+       "uuid":"UUID from the header section of manifest.json in My_RESOURCE_Pack",
+        "version":[1,0,0]
+      }
+   ]
+  }
